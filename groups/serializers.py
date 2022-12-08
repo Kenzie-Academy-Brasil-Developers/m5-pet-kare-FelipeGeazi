65d10316743c6ca5cf36ecd30e9709f4186fed1b
FelipeGeazi <felipegeaziartes@gmail.com>
2022-12-08 16:00:50 -0300

    corrigido

--- groups/serializers.py
+++ groups/serializers.py
@@ -6,7 +6,7 @@ from rest_framework.validators import UniqueValidator
 
 class GroupSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
-    scientific_name = serializers.SerializerMethodField(max_length=50)
+    scientific_name = serializers.CharField(max_length=50)
     created_at = serializers.DateTimeField(read_only=True)
 
 
